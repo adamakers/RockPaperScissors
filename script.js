0825b20f812f2c835af00639c4b219a7ec14bb2b
Adam Akers <adamrakers@gmail.com>
2022-02-07 22:26:53 -0700

    added gui interaction.  refactored playRound function, but still sloppy

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const buttons = document.querySelectorAll('.btn');
+
+const resultsBoardPara = document.querySelector('.game-results');
+const computerScoreCountEl = document.querySelector('.computer-score-num');
+const playerScoreCountEl = document.querySelector('.player-score-num');
+
+let score = {0:0, 1:0};// 0 is computer 1 is user
 
 // generate a random number between 1 and 3 and returns choice
 function randomSelection() {
@@ -12,56 +19,62 @@ function randomSelection() {
   }
 }
 
-// prompts user for selection
-function getPlayerSelection() {
-  let promptValue = prompt("Enter your selection").toLowerCase();
-
-  if (promptValue !== 'rock' && promptValue !== 'paper' && promptValue !== 'scissors') {
-    console.log('incorrect value.  try again');
-    return getPlayerSelection();
-  }
-
-  return promptValue;
-}
-
 //Plays round and determines winner
 function playRound(userSelection, computerSelection) {
   console.log('########################################################')
   console.log(`User chose: ${userSelection} |||| Computer chose: ${computerSelection}`);
   
   if (userSelection === computerSelection) {
-    return 'Round tied!';
+    return -1;
   }
 
+  // 0 if computer wins 1 if user wins
   if  (userSelection === 'rock' && computerSelection === 'paper') {
-    return "Computer wins!";
+    return 0;
   } else if (userSelection === 'rock' && computerSelection === 'scissors') {
-    return "User Wins!";
+    return 1;
   } else if (userSelection === 'paper' && computerSelection === 'scissors') {
-    return "Computer Wins!";
+    return 0;
   } else if (userSelection === 'paper' && computerSelection === 'rock') {
-    return "User Wins!";
+    return 1;
   } else if (userSelection === 'scissors' && computerSelection === 'paper') {
-    return "User Wins!"
+    return 1;
   } else if (userSelection === 'scissors' && computerSelection === 'rock') {
-    return "Computer Wins!";
+    return 0;
   }
 }
 
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = getPlayerSelection();
-    const computerSelection = randomSelection();
-
-    console.log(playRound(playerSelection, computerSelection));
-  }
-}
-
-game();
-
-
-
-
-
+function updateScoreboard(gameResults) {
   
+  if (gameResults === -1) {
+    resultsBoardPara.textContent = 'Game has Tied!';
+  } else if (gameResults === 0) {
+    score[0]++;
+    resultsBoardPara.textContent = 'Computer has won!';
+    computerScoreCountEl.textContent = score[0];
+  } else {
+    score[1]++;
+    resultsBoardPara.textContent = 'Player has won!';
+    playerScoreCountEl.textContent = score[1];
+  }
+  console.log(score);
+}
+
+
+function game(playerSelection) {
+  const computerSelection = randomSelection();
+  const results = playRound(playerSelection, computerSelection);
+  
+  console.log(results);
+  updateScoreboard(results);
+}
+
+
+buttons.forEach( btn => {
+  btn.addEventListener('click', function(e) {
+    const playerSelection = e.target.dataset.choice;
+
+    game(playerSelection);
+    
+  })
+})
